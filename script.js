f4bb1e9ddfb233b6eab65dd8416c2e60e4b24f3d
Aleksandra Lipert <ola.luszcz@interia.pl>
2019-02-06 22:32:53 +0100

    Add state and lifecycle components

--- script.js
+++ script.js
@@ -5,6 +5,46 @@ var Counter = React.createClass({
         };
     },
 
+    getDefultProps: function() {
+        console.log('Początkowy stan propsów ustawiony domyślnie')
+    },
+
+    getInitialState: function() {
+        console.log('Początkowy stan komponentu')
+    },
+
+    componentWillMount: function() {
+        console.log('Metody wywoływana zaraz przed wykonaniem metody render, na ekranie pokazuje stan początkowy')
+    },
+
+    react: function() {
+        console.log('Metoda, która odpowiada za wygląd komponentu podczas pierwszego wyświetlenie oraz po każdej zmianie, zwraca ReactElement')
+    },
+
+    componentDidMount: function() {
+        console.log('Aktualizacja stanu komponentu')
+    },
+
+    componentWillReceiveProps: function() {
+        console.log('Metoda wywoływana tylko wtedy gdy komponent otrzyma nowe właściwości')
+    },
+
+    shouldComponentUpdate: function() {
+        console.log('Metoda wywoływana tuż przed render, sprawdza czy rzeczywiście trzeba jeszcze raz zaktualizować komponent')
+    },
+
+    componentWillUpdate: function() {
+        console.log('Metoda przygotowująca na nadchdzące zmiany')
+    },
+
+    componentDidUpdate: function() {
+        console.log('Metoda aktualizująca stan komponentu - zmiany na DOM')
+    },
+
+    componentWillUnmount: function() {
+        console.log('Metoda kończąca życie komponentu, służy do ewentualnych zadań czyszczenia np. wyczyszczenie jakiś elementów DOM')
+    },
+
     increment: function() {
         this.setState({//metoda ustawienie stanu
             counter: this.state.counter + 1
@@ -27,5 +67,11 @@ var Counter = React.createClass({
     }
 });
 
-var element = React.createElement(Counter);
-ReactDOM.render(element, document.getElementById('app'));
+var elements = React.createElement('div', {},
+    React.createElement(Counter),
+    React.createElement(Counter),
+    React.createElement(Counter),
+)
+
+
+ReactDOM.render(elements, document.getElementById('app'));
